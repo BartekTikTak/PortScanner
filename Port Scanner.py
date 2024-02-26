b4e387153c6660d844994657529ef781c8a145f1
BartekTikTak <98836765+BartekTikTak@users.noreply.github.com>
2024-02-26 23:17:19 +0100

    Update Port Scanner.py

--- Port Scanner.py
+++ Port Scanner.py
@@ -11,14 +11,14 @@ def scan_port(target, port):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
         s.settimeout(1) 
         s.connect((target, port)) 
-        print(f"Port {port} Jest Otwarty na {target}") 
+        print(f"Port {port} Its open on {target}") 
         open_ports.append(port) 
     except: 
-        print(f"Port {port} Jest Zamkniety na {target}")
+        print(f"Port {port} Its close on {target}")
 
 for port in range(min_port, max_port + 1): 
     scan_port(target, port)
 
 with open("open_ports.txt", "w") as file: 
     for port in open_ports: 
-        file.write(f"Port {port} jest otwarty na {target}")
+        file.write(f"Port {port} Its open on {target}")
